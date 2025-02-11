2fa135d704ce725724e155334a1f46be3b69dbf7
Ahmad Koroma <ahmadkoroma25@outlook.com>
2025-02-11 02:41:32 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -19,8 +19,13 @@ User.find()
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
         .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist",
+                })
+            }
             res.json(user)
-            console.log(user)
+            
         })
         .catch(err => {
             res.status(500).json({
